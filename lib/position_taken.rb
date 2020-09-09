ef1965f35325e349527d30f1b66e44929002e54e
Michael B Ansong <michael.ansong7@gmail.com>
2020-09-09 14:46:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,7 @@
-board = [" "," "," "," "," "," "," "," "," "]
-def position_taken? (board, index)
-  taken = nil 
-  if board[index] ==  " " || board[index] == "" || board[index] == nil
-  taken = false
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
   else
-  taken = true
- end
+    return true
+  end
   
