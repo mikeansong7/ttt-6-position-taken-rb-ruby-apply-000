08b00c9c0162ec30db651cc040fd1cca55d4f6a1
Michael B Ansong <michael.ansong7@gmail.com>
2020-09-09 14:46:48 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+  return false
   else
-    return true
+  return true
   end
   
